8e07f8b2687822aa0ecba4d7a0b2ff765f694d14
ABRARAARNOB <106896794+ABRARAARNOB@users.noreply.github.com>
2026-09-14 01:27:23 +0600

    Update page.tsx

--- src/app/recruiter/page.tsx
+++ src/app/recruiter/page.tsx
@@ -841,7 +841,7 @@ export default function RecruiterDashboard() {
                   </label>
                   <input
                     type="text"
-                    required
+                   
                     value={editJob.location}
                     onChange={(e) => setEditJob({ ...editJob, location: e.target.value })}
                     className="w-full rounded-lg border border-slate-200 px-3.5 py-2.5 text-sm outline-none focus:border-[#3b28c8] transition"
